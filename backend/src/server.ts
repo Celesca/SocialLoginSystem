ac13378c603b74f7b1ba0cb532af92c40f0cdf99
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-24 03:14:14 +0700

    add: auth.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from 'express';
+const authRouter = require('./routes/auth');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res:Response) => {
-    res.send("Hello World!");
-})
+app.use('/', authRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
