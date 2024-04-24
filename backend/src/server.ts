35a1d92693c7e855741b75acb8598ed5a7dfa7df
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-25 01:00:24 +0700

    add: morgan and cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
 const authRouter = require('./routes/auth');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true
+}));
+app.use(morgan('dev'));
 app.use('/', authRouter);
 
 app.listen(port, () => {
